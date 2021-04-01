74b815c1bda121bfdf4e66227fe597038aa3dcbf
Sadiqur Sakib <sadiqursakib42@gmail.com>
2021-04-01 17:04:45 -0400

    divisible7

---
+++ Sadiqur.Sakib_Divisible7_2018.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int number;
+	cout<<"number: ";//user inputs a number
+	cin>>number;//takes the number and runs it thru the equation
+	if (number % 7 == 0)//equation for finding out wether or not number is divisible by 7
+	{
+		cout<<"its divisible by 7";
+    }
+	else//if its not divisible by seven this is printed
+	{
+		cout<<"its not divisible by 7";
+	}
+
+	
+	
+	
+	
+	
+}
+
+
+
